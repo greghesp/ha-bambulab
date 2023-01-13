b0055d173ac6d690e4008e18764d377a1b1b1835
Greg Hesp <greg.hesp@gmail.com>
2023-01-13 21:51:16 +0000

    Added model and prepare for AMS device

--- custom_components/bambu_lab/models.py
+++ custom_components/bambu_lab/models.py
@@ -7,19 +7,39 @@ from .coordinator import BambuDataUpdateCoordinator
 
 
 class BambuLabEntity(CoordinatorEntity[BambuDataUpdateCoordinator]):
-    """Defines a base WLED entity."""
+    """Defines a base Bambu entity."""
 
     _attr_has_entity_name = True
 
     @property
     def device_info(self) -> DeviceInfo:
-        """Return device information about this WLED device."""
+        """Return device information about this Bambu  device."""
 
-        LOGGER.debug(f"device info {self}")
+        # TODO: Populate with device name and serial
         return DeviceInfo(
             identifiers={
                 (DOMAIN, "123123")
             },
             name="Change",
+            model="X1C",
+            manufacturer="Bambu Lab",
+        )
+
+
+class AMSEntity(CoordinatorEntity[BambuDataUpdateCoordinator]):
+    """Defines a base AMS entity."""
+
+    _attr_has_entity_name = True
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device information about this AMS device."""
+
+        return DeviceInfo(
+            identifiers={
+                (DOMAIN, "AMS123")
+            },
+            name="AMS 1",
+            model="AMS",
             manufacturer="Bambu Lab",
         )
