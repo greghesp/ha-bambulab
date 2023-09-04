6e55fd05431eb54580a51314e12f2d51aa6b4a72
Greg Hesp <greg.hesp@gmail.com>
2023-09-04 22:25:21 +0100

    Camera stream works

--- custom_components/bambu_lab/camera.py
+++ custom_components/bambu_lab/camera.py
@@ -1,14 +1,15 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.core import HomeAssistant
+import yarl
+from homeassistant.components import ffmpeg
 
 from .const import DOMAIN, LOGGER
 from .models import BambuLabEntity
 from .pybambu.const import Features
 
-from homeassistant.components.camera import (
-    Camera
-)
+from homeassistant.components.camera import Camera, CameraEntityFeature
+
 from .coordinator import BambuDataUpdateCoordinator
 
 
@@ -19,10 +20,7 @@ async def async_setup_entry(
 ) -> None:
     coordinator: BambuDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
-    entities_to_add: list = []
-    # if coordinator.data.supports_feature(Features.CHAMBER_LIGHT):
-    #     entities_to_add.append(BambuLabChamberLight(coordinator, entry))
-    entities_to_add.append(BambuLabCamera(coordinator, entry))
+    entities_to_add: list = [BambuLabCamera(coordinator, entry)]
     async_add_entities(entities_to_add)
 
 
@@ -31,6 +29,8 @@ class BambuLabCamera(BambuLabEntity, Camera):
 
     _attr_translation_key = "camera"
     _attr_icon = "mdi:camera"
+    _attr_supported_features = CameraEntityFeature.STREAM
+    _attr_brand = "Bambu Lab"
 
     def __init__(
             self,
@@ -38,16 +38,40 @@ class BambuLabCamera(BambuLabEntity, Camera):
             config_entry: ConfigEntry
     ) -> None:
         """Initialize the sensor."""
+
         self._attr_unique_id = f"{config_entry.data['serial']}camera"
         super().__init__(coordinator=coordinator)
+        Camera.__init__(self)
 
     @property
     def is_streaming(self) -> bool:
-        return False
+        if self.coordinator.get_model().camera.rtsp_url == "disable":
+            return False
+        return True
 
     @property
     def is_recording(self) -> bool:
+        if self.coordinator.get_model().camera.recording == "enable":
+            return True
         return False
 
     async def stream_source(self) -> str | None:
-        return self.coordinator.get_model().camera.rtsp_url
+        url = yarl.URL(self.coordinator.get_model().camera.rtsp_url)
+        # TODO: Replace password with access code from config flow
+        url = url.with_user('bblp').with_password('xxxxxxx')
+        return str(url)
+
+    # TODO: async camera image doesn't work for some reason
+    async def async_camera_image(
+            self, width: int | None = None, height: int | None = None
+    ) -> bytes | None:
+        """Return a still image response from the camera."""
+        stream_source = await self.stream_source()
+        if not stream_source:
+            return None
+        return await ffmpeg.async_get_image(
+            self.hass,
+            stream_source,
+            width=width,
+            height=height,
+        )
