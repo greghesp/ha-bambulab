ab9ac99cadc485f19ae7a0fe4a99414b50288e88
Adrian Garside <adriangarside@outlook.com>
2023-09-23 10:02:38 -0700

    Delete non-functional mc_print support

--- custom_components/bambu_lab/definitions.py
+++ custom_components/bambu_lab/definitions.py
@@ -392,14 +392,6 @@ AMS_SENSORS: tuple[BambuLabSensorEntityDescription, ...] = (
         value_fn=lambda self: self.coordinator.get_model().ams.data[self.index].humidity_index
     ),
     BambuLabSensorEntityDescription(
-        key="humidity",
-        native_unit_of_measurement=PERCENTAGE,
-        device_class=SensorDeviceClass.HUMIDITY,
-        state_class=SensorStateClass.MEASUREMENT,
-        value_fn=lambda self: self.coordinator.get_model().ams.data[self.index].humidity,
-        exists_fn=lambda coordinator: coordinator.get_model().supports_feature(Features.AMS_RAW_HUMIDITY)
-    ),
-    BambuLabSensorEntityDescription(
         key="temperature",
         translation_key="ams_temp",
         native_unit_of_measurement=UnitOfTemperature.CELSIUS,
