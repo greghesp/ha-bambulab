e02d56813afd377d38c5b396c5655b1c2c5fa192
Adrian Garside <adriangarside@outlook.com>
2023-09-24 11:20:53 -0700

    Replace device_trigger.py with the developer script generated one

--- custom_components/bambu_lab/device_trigger.py
+++ custom_components/bambu_lab/device_trigger.py
@@ -1,85 +1,97 @@
+"""Provides device triggers for NEW_NAME."""
 from __future__ import annotations
+
+from typing import Any
 
 import voluptuous as vol
 
-from homeassistant.components.device_automation import TRIGGER_BASE_SCHEMA
-from homeassistant.components.device_automation.exceptions import (
-    InvalidDeviceAutomationConfig,
+from homeassistant.components.device_automation import DEVICE_TRIGGER_BASE_SCHEMA
+from homeassistant.components.homeassistant.triggers import state as state_trigger
+from homeassistant.const import (
+    CONF_DEVICE_ID,
+    CONF_DOMAIN,
+    CONF_ENTITY_ID,
+    CONF_PLATFORM,
+    CONF_TYPE,
+    STATE_OFF,
+    STATE_ON,
 )
-from homeassistant.const import CONF_DEVICE_ID, CONF_DOMAIN, CONF_TYPE, CONF_PLATFORM
 from homeassistant.core import CALLBACK_TYPE, HomeAssistant
-from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers import device_registry
-from homeassistant.helpers.trigger import TriggerActionType, TriggerInfo, event_trigger
+from homeassistant.helpers import config_validation as cv, entity_registry as er
+from homeassistant.helpers.trigger import TriggerActionType, TriggerInfo
 from homeassistant.helpers.typing import ConfigType
 
-from . import trigger
 from .const import DOMAIN, LOGGER
 
+# TODO specify your supported trigger types.
+TRIGGER_TYPES = {"turned_on", "turned_off"}
 
-TRIGGER_TYPES = { "something_happened " }
-TRIGGER_SCHEMA = TRIGGER_BASE_SCHEMA.extend(
+TRIGGER_SCHEMA = DEVICE_TRIGGER_BASE_SCHEMA.extend(
     {
+        vol.Required(CONF_ENTITY_ID): cv.entity_id,
         vol.Required(CONF_TYPE): vol.In(TRIGGER_TYPES),
     }
 )
 
 
-# async def async_validate_trigger_config(
-#     hass: HomeAssistant, config: ConfigType
-# ) -> ConfigType:
-#     """Validate config."""
-#     config = TRIGGER_SCHEMA(config)
-
-#     if config[CONF_TYPE] == TURN_ON_PLATFORM_TYPE:
-#         device_id = config[CONF_DEVICE_ID]
-#         try:
-#             device = async_get_device_entry_by_device_id(hass, device_id)
-#             if DOMAIN in hass.data:
-#                 async_get_client_by_device_entry(hass, device)
-#         except ValueError as err:
-#             raise InvalidDeviceAutomationConfig(err) from err
-
-#     return config
-
-
-async def async_get_triggers(hass, device_id):
-    """Return a list of triggers."""
-    LOGGER.debug("!!!!!!!!!!!!!!!!!!!! device_trigger::async_get_triggers")
-
-    device_registry = device_registry.async_get(hass)
-    device = device_registry.async_get(device_id)
-
+async def async_get_triggers(
+    hass: HomeAssistant, device_id: str
+) -> list[dict[str, Any]]:
+    """List device triggers for NEW_NAME devices."""
+    registry = er.async_get(hass)
     triggers = []
 
-    # Determine which triggers are supported by this device_id ...
+    LOGGER.debug("!!!!!!!!!!!!!!!!!!!! device_trigger::async_get_triggers")
 
-    triggers.append({
-        # Required fields of TRIGGER_BASE_SCHEMA
-        CONF_PLATFORM: "device",
-        CONF_DOMAIN: DOMAIN,
-        CONF_DEVICE_ID: device_id,
-        # Required fields of TRIGGER_SCHEMA
-        CONF_TYPE: "something_happened",
-    })
+    # TODO Read this comment and remove it.
+    # This example shows how to iterate over the entities of this device
+    # that match this integration. If your triggers instead rely on
+    # events fired by devices without entities, do something like:
+    # zha_device = await _async_get_zha_device(hass, device_id)
+    # return zha_device.device_triggers
+
+    # Get all the integrations entities for this device
+    for entry in er.async_entries_for_device(registry, device_id):
+        if entry.domain != DOMAIN:
+            continue
+
+        # Add triggers for each entity that belongs to this integration
+        # TODO add your own triggers.
+        base_trigger = {
+            CONF_PLATFORM: "device",
+            CONF_DEVICE_ID: device_id,
+            CONF_DOMAIN: DOMAIN,
+            CONF_ENTITY_ID: entry.entity_id,
+        }
+        triggers.append({**base_trigger, CONF_TYPE: "turned_on"})
+        triggers.append({**base_trigger, CONF_TYPE: "turned_off"})
 
     return triggers
 
 
-async def async_attach_trigger(hass, config, action, trigger_info):
+async def async_attach_trigger(
+    hass: HomeAssistant,
+    config: ConfigType,
+    action: TriggerActionType,
+    trigger_info: TriggerInfo,
+) -> CALLBACK_TYPE:
     """Attach a trigger."""
+    # TODO Implement your own logic to attach triggers.
+    # Use the existing state or event triggers from the automation integration.
+
     LOGGER.debug("!!!!!!!!!!!!!!!!!!!! device_trigger::async_attach_trigger")
 
-    event_config = event_trigger.TRIGGER_SCHEMA(
-        {
-            event_trigger.CONF_PLATFORM: "event",
-            event_trigger.CONF_EVENT_TYPE: "mydomain_event",
-            event_trigger.CONF_EVENT_DATA: {
-                CONF_DEVICE_ID: config[CONF_DEVICE_ID],
-                CONF_TYPE: config[CONF_TYPE],
-            },
-        }
-    )
-    return await event_trigger.async_attach_trigger(
-        hass, event_config, action, trigger_info, platform_type="device"
+    if config[CONF_TYPE] == "turned_on":
+        to_state = STATE_ON
+    else:
+        to_state = STATE_OFF
+
+    state_config = {
+        state_trigger.CONF_PLATFORM: "state",
+        CONF_ENTITY_ID: config[CONF_ENTITY_ID],
+        state_trigger.CONF_TO: to_state,
+    }
+    state_config = await state_trigger.async_validate_trigger_config(hass, state_config)
+    return await state_trigger.async_attach_trigger(
+        hass, state_config, action, trigger_info, platform_type="device"
     )
