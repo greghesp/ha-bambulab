97a117a93cb742aa6f713a81cd20e07a6b4031b9
Adrian Garside <adriangarside@outlook.com>
2023-10-26 08:11:02 -0700

    Update device type to have no spaces, enable k_value usage and generated start time

--- custom_components/bambu_lab/pybambu/models.py
+++ custom_components/bambu_lab/pybambu/models.py
@@ -79,11 +79,11 @@ class Device:
             case Features.EXTERNAL_SPOOL:
                 return True
             case Features.K_VALUE:
-                return self.info.device_type == "P1P" or self.info.device_type == "P1S"
+                return self.info.device_type == "P1P" or self.info.device_type == "P1S" or self.info.device_type == "A1Mini"
             case Features.START_TIME:
                 return self.info.device_type == "X1" or self.info.device_type == "X1C"
             case Features.START_TIME_GENERATED:
-                return self.info.device_type == "P1P" or self.info.device_type == "P1S"
+                return self.info.device_type == "P1P" or self.info.device_type == "P1S" or self.info.device_type == "A1Mini"
             case Features.AMS_TEMPERATURE:
                 return self.info.device_type == "X1" or self.info.device_type == "X1C"
             case Features.CAMERA_RTSP:
