b09fdfc82d36cf69b75ac29514b1e4c73418d5e8
Adrian Garside <adriangarside@outlook.com>
2023-12-25 15:26:37 -0800

    Various fixes

--- custom_components/bambu_lab/diagnostics.py
+++ custom_components/bambu_lab/diagnostics.py
@@ -13,8 +13,14 @@ from .pybambu.commands import PUSH_ALL, GET_VERSION
 
 
 TO_REDACT = [
+    "access_code",
+    "auth_token",
+    "email",
     "rtsp_url",
-    "sn"
+    "serial",
+    "sn",
+    "title",
+    "username"
 ]
 
 
@@ -24,10 +30,14 @@ async def async_get_config_entry_diagnostics(
     """Return diagnostics for a config entry."""
     coordinator: BambuDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
 
+    # Must convert this to a dict for redaction to work correctly. Redaction leaves empty values as empty so we know if a value was present or not.
+    entry = coordinator._entry.as_dict()
+
     coordinator.client.publish(PUSH_ALL)
     coordinator.client.publish(GET_VERSION)
 
     diagnostics_data = {
+        "config_entry": async_redact_data(entry, TO_REDACT),
         "push_all": async_redact_data(coordinator.data.push_all_data, TO_REDACT),
         "get_version": async_redact_data(coordinator.data.get_version_data, TO_REDACT),
     }
