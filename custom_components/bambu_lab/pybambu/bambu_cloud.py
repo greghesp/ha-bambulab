3c37c1bba839604ecc21a03b8b8ebd8bf7475f48
Adrian Garside <adriangarside@outlook.com>
2023-12-25 16:32:10 -0800

    Don't log access code to debug logs

--- custom_components/bambu_lab/pybambu/bambu_cloud.py
+++ custom_components/bambu_lab/pybambu/bambu_cloud.py
@@ -110,7 +110,6 @@ class BambuCloud:
         if not response.ok:
             LOGGER.debug(f"Received error: {response.status_code}")
             raise ValueError(response.status_code)
-        LOGGER.debug(f"Success: {response.json()}")
         return response.json()['devices']
 
     # The task list is of the following form with a 'hits' array with typical 20 entries.
