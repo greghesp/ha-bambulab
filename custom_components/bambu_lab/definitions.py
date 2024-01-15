d53eb09f9344f4ae1ea755bf29a13dd6c34a791d
Adrian Garside <adriangarside@outlook.com>
2024-01-15 15:27:09 -0800

    Set default precision of to rounded hours for new sensor

--- custom_components/bambu_lab/definitions.py
+++ custom_components/bambu_lab/definitions.py
@@ -262,6 +262,7 @@ PRINTER_SENSORS: tuple[BambuLabSensorEntityDescription, ...] = (
         translation_key="total_usage_hours",
         icon="mdi:clock",
         native_unit_of_measurement=UnitOfTime.HOURS,
+        suggested_display_precision=0,
         available_fn=lambda self: self.coordinator.get_model().info.usage_hours is not None,
         value_fn=lambda self: self.coordinator.get_model().info.usage_hours,
     ),
