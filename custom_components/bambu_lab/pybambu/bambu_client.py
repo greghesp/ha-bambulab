fe8cd76d00d51feba3283e0c06e251570f9e9537
Johan Kanflo <johan.kanflo@bitfuse.net>
2024-11-07 05:26:30 +0100

    Starting the camera is optional, defaults to on (#696)

--- custom_components/bambu_lab/pybambu/bambu_client.py
+++ custom_components/bambu_lab/pybambu/bambu_client.py
@@ -286,7 +286,7 @@ class BambuClient:
     _usage_hours: float
 
     def __init__(self, device_type: str, serial: str, host: str, local_mqtt: bool, region: str, email: str,
-                 username: str, auth_token: str, access_code: str, usage_hours: float = 0, manual_refresh_mode: bool = False):
+                 username: str, auth_token: str, access_code: str, usage_hours: float = 0, manual_refresh_mode: bool = False, chamber_image: bool = True):
         self.callback = None
         self.host = host
         self._local_mqtt = local_mqtt
@@ -303,6 +303,7 @@ class BambuClient:
         self._device = Device(self)
         self.bambu_cloud = BambuCloud(region, email, username, auth_token)
         self.slicer_settings = SlicerSettings(self)
+        self.use_chamber_image = chamber_image
 
     @property
     def connected(self):
@@ -381,9 +382,10 @@ class BambuClient:
 
         if not self._device.supports_feature(Features.CAMERA_RTSP):
             if self._device.supports_feature(Features.CAMERA_IMAGE):
-                LOGGER.debug("Starting Chamber Image thread")
-                self._camera = ChamberImageThread(self)
-                self._camera.start()
+                if self.use_chamber_image:
+                    LOGGER.debug("Starting Chamber Image thread")
+                    self._camera = ChamberImageThread(self)
+                    self._camera.start()
             elif (self.client.host == "") or (self.client._access_code == ""):
                 LOGGER.debug("Skipping camera setup as local access details not provided.")
 
