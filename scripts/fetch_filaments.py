b32da96a9feb1655242705bdbfb99c93c74d8608
AdrianGarside <adriangarside@outlook.com>
2024-11-17 14:41:44 -0800

    Update filaments (#752)

--- scripts/fetch_filaments.py
+++ scripts/fetch_filaments.py
@@ -4,14 +4,17 @@ python3 scripts/fetch_filaments.py > custom_components/bambu_lab/pybambu/filamen
 
 from __future__ import annotations
 
+import cloudscraper
 import json
-import requests
-
+import os
 
 def get_filaments() -> dict:
-    url = 'https://api.bambulab.com/v1/iot-service/api/slicer/setting?version=undefined'
-    response = requests.get(url, timeout=10)
+    url = 'https://api.bambulab.com/v1/iot-service/api/slicer/setting?version=1.10.0.89'
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url, timeout=10)
     if not response.ok:
+        if 'cloudflare' in response.text:
+            print("CLOUDFLARED")
         raise ValueError(response.status_code)
     return response.json()["filament"]["public"]
 
@@ -19,4 +22,8 @@ def get_filaments() -> dict:
 filaments = get_filaments()
 data = {f["filament_id"]: f["name"].split("@", 1)[0].strip() for f in get_filaments()}
 
-print(json.dumps(data, indent=4, sort_keys=True))
+output = json.dumps(data, indent=4, sort_keys=True)
+
+script_path = os.path.dirname(__file__)
+with open(f'{script_path}/../custom_components/bambu_lab/pybambu/filaments.json', 'w') as file:
+    file.write(output)
