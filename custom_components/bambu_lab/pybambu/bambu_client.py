83e4f8a4c7c1d02e7eeda98181bf47cf5615f0d9
Adrian Garside <adriangarside@outlook.com>
2025-02-01 16:15:28 -0800

    Default FTP to on for local_mqtt connections

--- custom_components/bambu_lab/pybambu/bambu_client.py
+++ custom_components/bambu_lab/pybambu/bambu_client.py
@@ -338,7 +338,7 @@ class BambuClient:
         self._usage_hours = config.get('usage_hours', 0)
         self._username = config.get('username', '')
         self._enable_camera = config.get('enable_camera', True)
-        self._enable_ftp = config.get('enable_ftp', False)
+        self._enable_ftp = config.get('enable_ftp', self._local_mqtt)
 
         self._connected = False
         self._port = 1883
