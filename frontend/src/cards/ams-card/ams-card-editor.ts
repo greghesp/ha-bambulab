9d2d2ec80ce97762f3165b625880b28e37fccbb8
Greg Hesp <greg.hesp@gmail.com>
2025-02-06 20:06:09 +0000

    Update example card, attempt to configure AMS

--- frontend/src/cards/ams-card/ams-card-editor.ts
+++ frontend/src/cards/ams-card/ams-card-editor.ts
@@ -2,6 +2,7 @@ import { AMS_CARD_EDITOR_NAME, AMS_MODELS } from "./const";
 import { INTEGRATION_DOMAIN, MANUFACTURER } from "../../const";
 import { customElement, state } from "lit/decorators.js";
 import { LitElement, html, nothing } from "lit";
+import memoizeOne from "memoize-one";
 
 // https://www.home-assistant.io/docs/blueprint/selectors/#select-selector
 const filterCombinations = AMS_MODELS.map((model) => ({
@@ -9,43 +10,50 @@ const filterCombinations = AMS_MODELS.map((model) => ({
   model: model,
 }));
 
-const NEW_SCHEMA = [
-  { name: "header", label: "Card Header", selector: { text: {} } },
-  { name: "subtitle", label: "Subtitle", selector: { text: {} } },
-  {
-    name: "ams",
-    label: "AMS",
-    selector: { device: { filter: filterCombinations } },
-  },
-  {
-    name: "style",
-    label: "Card Style",
-    selector: {
-      select: {
-        options: [
-          { label: "Vector", value: "vector" },
-          { label: "Graphic", value: "graphic" },
-        ],
-      },
-    },
-  },
-];
-
 @customElement(AMS_CARD_EDITOR_NAME)
 export class AmsCardEditor extends LitElement {
   @state() private _config?;
   @state() private hass: any;
 
-  static get properties() {
-    return {
-      hass: {},
-      _config: { state: true },
-    };
-  }
-
   public setConfig(config): void {
     this._config = config;
   }
+
+  private _schema = memoizeOne((showHeader: boolean) => [
+    {
+      name: "header",
+      label: "Card Header",
+      selector: { text: {} },
+      required: false,
+    },
+    { name: "show_header", label: "Show Header", selector: { boolean: false } },
+    ...(showHeader
+      ? [
+          {
+            name: "subtitle",
+            label: "Subtitle",
+            selector: { text: {} },
+          },
+        ]
+      : ""),
+    {
+      name: "ams",
+      label: "AMS",
+      selector: { device: { filter: filterCombinations } },
+    },
+    {
+      name: "style",
+      label: "Card Style",
+      selector: {
+        select: {
+          options: [
+            { label: "Vector", value: "vector" },
+            { label: "Graphic", value: "graphic" },
+          ],
+        },
+      },
+    },
+  ]);
 
   _handleValueChanged(ev) {
     const messageEvent = new CustomEvent("config-changed", {
@@ -57,13 +65,18 @@ export class AmsCardEditor extends LitElement {
   }
 
   render() {
+    const schema = this._schema(this._config!.header !== undefined);
+    const data = {
+      show_header: this._config!.header !== undefined,
+      ...this._config,
+    };
     return html`
       <div>
         <ha-form
           .hass=${this.hass}
           .data=${this._config}
-          .schema=${NEW_SCHEMA}
-          .computeLabel=${(schema) => schema.label}
+          .schema=${schema}
+          .computeLabel=${(s) => s.label}
           @value-changed=${this._handleValueChanged}
         ></ha-form>
       </div>
