dce4a5419306520f2dd2175887c82ed57783a43f
Greg Hesp <768908+greghesp@users.noreply.github.com>
2025-02-08 20:17:23 +0000

    Detach frontend source (#946)
    * Update publish.yml
    
    * Remove frontend

--- frontend/src/cards/ams-card/const.ts
+++
@@ -1,5 +0,0 @@
-import { PREFIX_NAME } from "../../const";
-
-export const AMS_CARD_NAME = `${PREFIX_NAME}-ams-card`;
-export const AMS_CARD_EDITOR_NAME = `${AMS_CARD_NAME}-editor`;
-export const AMS_MODELS = ["AMS", "AMS Lite"];
