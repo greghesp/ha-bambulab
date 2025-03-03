1ce6f52606fa2712d4d21138ef329045c164746e
AdrianGarside <adriangarside@outlook.com>
2025-03-02 21:55:06 -0800

    Fix error text always being unknown (#1106)
    * Initial draft of set filament service call
    
    * Add temperatures
    
    * More FTP model download robustness improvements
    
    * Fix bad merge
    
    * Fix print error code text always being unknown

--- custom_components/bambu_lab/pybambu/utils.py
+++ custom_components/bambu_lab/pybambu/utils.py
@@ -100,6 +100,7 @@ def get_current_stage(id) -> str:
 
 def get_HMS_error_text(code: str, language: str):
     """Return the human-readable description for an HMS error"""
+    code = code.replace("_", "")
     error = HMS_ERRORS.get(code, 'unknown')
     if '' == error:
         return 'unknown'
@@ -108,6 +109,7 @@ def get_HMS_error_text(code: str, language: str):
 
 def get_print_error_text(code: str, language: str):
     """Return the human-readable description for a print error"""
+    code = code.replace("_", "")
     error = PRINT_ERROR_ERRORS.get(code, 'unknown')
     if '' == error:
         return 'unknown'
