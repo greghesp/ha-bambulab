6ab8ee4838c77c75dcf5a49407f0f05bc2c8ae56
Adrian Garside <adriangarside@outlook.com>
2025-06-01 11:41:53 -0700

    Fix H2D developer lan mode determination

--- custom_components/bambu_lab/pybambu/models.py
+++ custom_components/bambu_lab/pybambu/models.py
@@ -236,8 +236,8 @@ class Device:
             if self.info.sw_ver == "unknown":
                 return False
             
-            if (self.info.device_type == Printers.H2D):
-                return not self.info.developer_lan_mode
+            if (self.info.device_type == Printers.H2D) and self.supports_sw_version("01.01.01.00"):
+                return not self.info.developer_lan_mode 
             elif (self.info.device_type == Printers.X1 or self.info.device_type == Printers.X1C) and self.supports_sw_version("01.08.50.18"):
                 return not self.info.developer_lan_mode
             elif (self.info.device_type == Printers.P1S or self.info.device_type == Printers.P1P) and self.supports_sw_version("01.07.50.18"):
