09005e7681d448e15101a62f335e6229a003f05d
AdrianGarside <adriangarside@outlook.com>
2025-07-12 17:32:45 -0700

    Fix laser module detection (#1455)

--- custom_components/bambu_lab/definitions.py
+++ custom_components/bambu_lab/definitions.py
@@ -194,7 +194,7 @@ PRINTER_SENSORS: tuple[BambuLabSensorEntityDescription, ...] = (
         translation_key="tool_module",
         icon="mdi:printer-3d-nozzle",
         device_class=SensorDeviceClass.ENUM,
-        options=["none", "laser", "cutter"],
+        options=["none", "laser10", "laser40", "cutter"],
         value_fn=lambda self: self.coordinator.get_model().extruder_tool.state,
         exists_fn=lambda coordinator: coordinator.get_model().supports_feature(Features.EXTRUDER_TOOL),
     ),
